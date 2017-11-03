6de90d8d68dccda9f8cd4c854f930dbe3a929c20
Bettybettybetty <879110783@qq.com>
2017-11-03 23:06:28 +0800

    change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
 	<div>
 		<home-header/>
 		<home-swiper></home-swiper>
+		<home-activity></home-activity>
 	</div>
 </template>
 
 <script>
 	import HeaderComponent from "./Header"
 	import swiper from "./Swiper"
+	import ActivityComponent from "./Activity"
 	
 	export default {
 		components: {
 			"home-header": HeaderComponent,
-			"home-swiper": swiper
+			"home-swiper": swiper,
+			"home-activity": ActivityComponent
 		}
 	}
 </script>
