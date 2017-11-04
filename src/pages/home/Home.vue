bcdaef70365cd76ca5122f8580c2165298f8c95b
Bettybettybetty <879110783@qq.com>
2017-11-04 15:42:24 +0800

    wek

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -10,12 +10,13 @@
 </template>
 
 <script>
-	import HeaderComponent from "./Header"
-	import swiper from "./Swiper"
+	import HeaderComponent from "./Header";
+	import swiper from "./Swiper";
 	import 	Mpswiper from "./Mpswiper";
-	import ActivityComponent from "./Activity"
-	import Hot_recommendation from "./Hot"import WeekendComponent from "./Weekend"
-	import WeekendComponent from "./Weekend"
+	import ActivityComponent from "./Activity";
+	import Hot_recommendation from "./Hot";
+	import WeekendComponent from "./Weekend";
+	
 	
 	export default {
 		created: function() {
