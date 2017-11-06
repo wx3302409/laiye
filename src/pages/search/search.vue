b35f6257d95c36d5edfd1f2d65eb841edb324117
wx3302409 <wangxinarm@163.com>
2017-11-06 15:41:59 +0800

    addSearch

---
+++ src/pages/search/search.vue
@@ -0,0 +1,24 @@
+<template>
+  	<div>
+    	<home-header/>
+    	<search-content></search-content>
+	</div>
+</template>
+
+<script>
+
+	import  HeaderComponent from "../home/Header";
+	import  SearchContent from "./SearchContent";
+
+	export default {
+
+		components: {
+			"home-header": HeaderComponent,
+			"search-content": SearchContent
+		}
+	}
+
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+import Search from '@/pages/search/search'
 
 Vue.use(Router)
 
 export default new Router({
+
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
     }
-  ]
+ ]
+
 })
